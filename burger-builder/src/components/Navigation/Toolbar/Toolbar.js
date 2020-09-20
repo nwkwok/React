0c87fa27828e5c8c1a69acdae83eb283cd826b90
nwkwok <nwkwok@gmail.com>
2020-09-20 15:34:14 -0700

    make sidebar toggle

--- burger-builder/src/components/Navigation/Toolbar/Toolbar.js
+++ burger-builder/src/components/Navigation/Toolbar/Toolbar.js
@@ -2,10 +2,11 @@ import React from 'react'
 import classes from './Toolbar.module.css'
 import Logo from '../../Logo/Logo'
 import NavigationItems from '../NavigationItems/NavigationItems'
+import MenuButton from './MenuButton/MenuButton'
 
-const Toolbar = () => (
+const Toolbar = (props) => (
     <header className={classes.Toolbar}>
-        <div>MENU</div>
+        <MenuButton clicked={props.openSideBar}>Menu</MenuButton>
         <div className={classes.Logo}>
         <Logo />
         </div>
