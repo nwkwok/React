afb949870b41ceb4729e00db3e653262d8357711
nwkwok <nwkwok@gmail.com>
2020-09-20 16:21:33 -0700

    restructure folders

--- burger-builder/src/App.js
+++ burger-builder/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.module.css';
-import Layout from './components/layouts/Layout'
+import Layout from './hoc/Layout/Layout'
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 
 class App extends Component {
