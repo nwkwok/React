78fe6fce60e2d809b13c636c176f47f32c3f435e
nwkwok <nwkwok@gmail.com>
2020-09-25 00:06:08 -0700

    add onChange and onSubmit functionality to push order to firebase

--- burger-builder/src/containers/Checkout/ContactData/ContactData.js
+++ burger-builder/src/containers/Checkout/ContactData/ContactData.js
@@ -7,51 +7,118 @@ import Input from '../../../components/UI/Input/Input'
 
 class ContactData extends Component {
     state = {
-        name: '',
-        email: '',
-        address: {
-            street: '',
-            postalCode: ''
-        },
-        loading: false
+        orderForm: {
+            name: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Your Name'
+                },
+                value: ''
+            },
+            street: {
+                    elementType: 'input',
+                    elementConfig: {
+                        type: 'text',
+                        placeholder: 'Street'
+                    },
+                    value: ''
+            },
+            zipCode: {
+                    elementType: 'input',
+                    elementConfig: {
+                        type: 'text',
+                        placeholder: 'Zip Code'
+                    },
+                    value: ''
+            },
+            country: {
+                    elementType: 'input',
+                    elementConfig: {
+                        type: 'text',
+                        placeholder: 'Country'
+                    },
+                    value: ''
+            },
+            email: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Your E-mail'
+                },
+                value: ''
+            },
+            deliveryMethod: {
+                elementType: 'select',
+                elementConfig: {
+                    options: [
+                        {value: 'fastest', displayValue: 'Fastest'},
+                        {value: 'cheapest', displayValue: 'Cheapest'},
+                    ]
+                },
+                value: ''  
+            }  
+            },
+    loading: false
     }
 
+
     orderHandler = (event) => {
-   
-    this.setState ({loading: true});
-    const order = {
-        ingredients: this.props.ingredients,
-        price: this.props.price,
-        customer: {
-            name: 'Nick Kwok',
-            address: {
-                street: '111 Test St',
-                zipCode: '12315',
-                state: 'CA'
-            },
-            email: 'test@test.com'
-        },
-        deliveryMethod: 'fastest'
+        event.preventDefault();
+        this.setState ({loading: true});
+        const formData = {};
+        for (let formElementIdentifier in this.state.orderForm) {
+            formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value;
         }
+        const order = {
+            ingredients: this.props.ingredients,
+            price: this.props.price,
+            orderData: formData
+        }
+        
     axios.post('/orders.json', order)
         .then(response => {
             this.setState({ loading: false})
             this.props.history.push('/')})
         .catch(error => {
             this.setState({ loading: false})});
-        event.preventDefault();
-        console.log(this.props.ingredients);
+    }
+
+    inputChangedHandler = (event, inputIdentifier) => {
+        const updatedOrderForm = {
+            ...this.state.orderForm
+        };
+        const updatedFormElement= {
+            ...updatedOrderForm[inputIdentifier]
+        };
+
+        updatedFormElement.value = event.target.value;
+        updatedOrderForm[inputIdentifier] = updatedFormElement
+        this.setState({orderForm: updatedOrderForm});
     }
 
     render () {
 
+        const formElementsArray = []
+        for (let key in this.state.orderForm) {
+            formElementsArray.push({
+                id: key,
+                config: this.state.orderForm[key]
+            });
+        }
+
         let form = (
-            <form>
-            <Input inputtype="input" type ="text" name="name" placeholder="Your name" />
-            <Input inputtype="input" type ="email" name="email" placeholder="Your email" />
-            <Input inputtype="input" type ="text" name="street" placeholder="Street" />
-            <Input inputtype="input" type ="text" name="postal" placeholder="Postal Code" />
-            <Button clicked={this.orderHandler} btnType="Success">ORDER</Button>
+            <form onSubmit={this.orderHandler}>
+            {formElementsArray.map(formElement => (
+                <Input  
+                    key={formElement.id}
+                    elementType={formElement.config.elementType}
+                    elementConfig={formElement.config.elementConfig}
+                    value={formElement.config.value}
+                    changed={(event) => this.inputChangedHandler(event, formElement.id)} />
+            ))}
+
+            <Button btnType="Success">ORDER</Button>
             </form>
         );
         if (this.state.loading) {
