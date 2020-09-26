6f834aa999940c9f8cb2dbc17860acc3a23c1fcb
nwkwok <nwkwok@gmail.com>
2020-09-25 21:44:53 -0700

    add form validation checks

--- burger-builder/src/components/UI/Input/Input.js
+++ burger-builder/src/components/UI/Input/Input.js
@@ -3,11 +3,16 @@ import classes from './Input.module.css'
 
 const Input = (props) => {
     let inputElement = null;
+    const inputClasses = [classes.InputElement];
+
+    if (props.invalid && props.shouldValidate) {
+        inputClasses.push(classes.Invalid);
+    }
 
     switch (props.elementType) {
         case('input'):
             inputElement = <input 
-            className={classes.InputElement}
+            className={inputClasses.join(' ')}
             {...props.elementConfig} 
             value={props.value}
             onChange={props.changed}/>
