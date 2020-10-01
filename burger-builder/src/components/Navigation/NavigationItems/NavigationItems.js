dbf7be037a58aa7ddb7a9832c5df9bcd14606252
nwkwok <nwkwok@gmail.com>
2020-10-01 13:47:48 -0700

    fix active link

--- burger-builder/src/components/Navigation/NavigationItems/NavigationItems.js
+++ burger-builder/src/components/Navigation/NavigationItems/NavigationItems.js
@@ -4,7 +4,7 @@ import NavigationItem from './NavigationItem/NavigationItem'
 
 const NavigationItems = (props) => (
     <ul className={classes.NavigationItems}>
-        <NavigationItem link="/">Burger Builder</NavigationItem>
+        <NavigationItem link="/" exact>Burger Builder</NavigationItem>
         {props.isAuthenticated ? <NavigationItem link="/orders">Orders</NavigationItem> : null}
        
         { !props.isAuthenticated  
